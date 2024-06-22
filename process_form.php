cfda3f87983bc689a93bf864b0a18131ddac56ae
cosmin <50593959+cosmin-andrei@users.noreply.github.com>
2024-06-22 16:30:15 +0300

    Update process_form.php

--- process_form.php
+++ process_form.php
@@ -1,16 +1,12 @@
 <?php
-// Includerea fișierului de configurare
 include 'config.php';
 
-// Crearea conexiunii la baza de date
 $conn = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
 
-// Verificarea conexiunii
 if ($conn->connect_error) {
     die("Conexiunea a eșuat: " . $conn->connect_error);
 }
 
-// Preluarea datelor din formular
 $email = $_POST['email'];
 $name = $_POST['name'];
 $phone = $_POST['phone'];
@@ -22,19 +18,16 @@ $criteria = $_POST['criteria'];
 $project = $_POST['project'];
 $additional = $_POST['additional'];
 
-// Verificarea dacă emailul există deja în baza de date
 $sql = "SELECT * FROM jurati WHERE email='$email'";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
     echo "Ai completat deja acest formular.";
 } else {
-    // Insert datelor în baza de date
     $sql = "INSERT INTO jurati (email, name, phone, social, category, recommendation, motivation, criteria, project, additional)
             VALUES ('$email', '$name', '$phone', '$social', '$category', '$recommendation', '$motivation', '$criteria', '$project', '$additional')";
 
     if ($conn->query($sql) === TRUE) {
-        // Trimiterea emailurilor
         $to = $email . ', aplicatii@ivoluntar.org';
         $subject = "Confirmare aplicare jurat Gala Voluntariatului";
         $message = "Multumim pentru aplicarea ca jurat la Gala Voluntariatului.\n\n"
@@ -50,11 +43,10 @@ if ($result->num_rows > 0) {
                  . "Proiect: $project\n"
                  . "Informatii suplimentare: $additional";
 
-        $headers = "From: no-reply@gala.ivoluntar.org";
+        $headers = "From: contact@ivoluntar.org";
 
         mail($to, $subject, $message, $headers);
 
-        // Afișarea mesajului de confirmare
         echo "Formularul a fost trimis cu succes. Vei primi un email de confirmare.";
     } else {
         echo "Eroare: " . $sql . "<br>" . $conn->error;
